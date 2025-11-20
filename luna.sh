d2d05ad9b1ad8451a23ebd859f9717841d0d2a80
Kenaidi01 <andrianp920@gmail.com>
2025-11-20 07:35:04 +0700

    Fix clone URL for local manifests repository

--- luna.sh
+++ luna.sh
@@ -5,7 +5,7 @@ rm -rf .repo/local_manifests/ prebuilts/clang/host/linux-x86
 repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs
 
 # clone local manifests
-git clone https://github.com/Kenskuyy-Prjkt/local_manifest.git -b lunaris .repo/local_manifests
+git clone https://github.com/Kenskuyy-Prjkt/local_manifests.git -b lunaris .repo/local_manifests
 
 # repo sync
 [ -f /usr/bin/resync ] && /usr/bin/resync || /opt/crave/resync.sh
